087a72b69dd76281cd737c400e22e0bbfa1edd6f
sairin <siriel88@gmail.com>
2020-04-06 21:03:27 +0300

    Work complete!

--- Exercises/5-curry.js
+++ Exercises/5-curry.js
@@ -7,10 +7,24 @@ const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 // Define function curry that accepts the length of the function
 // (amount of function arguments) and the function.
 
-const curry = (length, fn) => (...args) => null;
+const curry = (length, fn) => (...args) => {
+  if (length > args.length) {
+    const nf = fn.bind(null, ...args);
+    const nlen = length - args.length;
+    return curry(nlen, nf);
+  } else {
+    return fn(...args);
+  }
+};
 
 // Allows to enter pin code by one character,
 // Usage: press('3')('4')('5')('6')
 const press = curry(4, checkPin);
+
+console.log(press('3', '4', '5', '6'));
+console.log(press('3')('4')('5')('6'));
+console.log(press('4')('9')('6')('7'));
+console.log(press('4', '9')('6')('7'));
+console.log(press('4', '9')('6')('7', '8'));
 
 module.exports = { press };
--- Exercises/6-chaining.js
+++ Exercises/6-chaining.js
@@ -10,6 +10,21 @@ const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 //
 // For hint use https://github.com/HowProgrammingWorks/Cheatsheet
 
-const press = null;
+const press = char => ({
+  input: [char],
+  press(char) {
+    this.input.push(char);
+    if (this.input.length >= 4) {
+      return checkPin(...this.input);
+    } else {
+      return this;
+    }
+  },
+});
+
+console.log(checkPin);
+console.log(press('3').press('4').press('5').press('6'));
+console.log(press('4').press('9').press('6').press('7'));
+
 
 module.exports = { press };
